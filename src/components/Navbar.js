3b9a41879ed83929290ef7a9a88dd94fe1b9acb9
Somendra Pal Gupta <42165678+somendrapal@users.noreply.github.com>
2023-02-25 11:21:45 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -95,6 +95,16 @@ function NavBar() {
               
 
            
+            <Nav.Item className="fork-btn">
+              <Button
+                href="https://github.com/somendrapal/Portfolio"
+                target="_blank"
+                className="fork-btn-inner"
+              >
+                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+                <AiFillStar style={{ fontSize: "1.1em" }} />
+              </Button>
+            </Nav.Item>
 
             
           </Nav>
